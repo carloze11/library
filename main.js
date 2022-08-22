ffa351df5e9749d8fba7345ec8e798708221a6ab
Carlos Estrada <cestrada1109@gmail.com>
2022-08-21 21:32:42 -0500

    Add constructor

---
+++ main.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+
+function Book(title, author, pages, read){
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = () => {
+    return `${title} by ${author}, ${pages} pages, ${read}`
+  }
+}
+
+function addBookToLibrary(){
+  
+}
